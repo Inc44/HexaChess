1f48910b65b167e38fba86ad6bbd01af3ab35b25
Inc44 <incognita44@outlook.com>
2026-01-02 09:39:31 +0100

    sidebar

--- src/main/java/im/bpu/hexachess/MainWindow.java
+++ src/main/java/im/bpu/hexachess/MainWindow.java
@@ -2,19 +2,45 @@ package im.bpu.hexachess;
 
 import im.bpu.hexachess.ui.HexPanel;
 
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 public class MainWindow {
 	private HexPanel hexPanel;
 	@FXML private Button settingsHelpButton;
+	@FXML private VBox sidebar;
 	@FXML private Canvas canvas;
 	@FXML
 	private void initialize() {
 		hexPanel = new HexPanel(canvas, State.getState());
+		sidebar.setTranslateX(-160);
+		sidebar.setVisible(false);
+	}
+	@FXML
+	private void toggleSidebar() {
+		boolean closed = !sidebar.isVisible();
+		Duration duration = Duration.millis(160);
+		TranslateTransition transition = new TranslateTransition(duration, sidebar);
+		if (closed) {
+			transition.setToX(0);
+			sidebar.setVisible(true);
+		} else {
+			transition.setToX(-160);
+			transition.setOnFinished(ev -> sidebar.setVisible(false));
+		}
+		transition.play();
+	}
+	@FXML
+	private void onBoardClicked() {
+		if (sidebar.isVisible()) {
+			toggleSidebar();
+		}
 	}
 	@FXML
 	private void restart() {
