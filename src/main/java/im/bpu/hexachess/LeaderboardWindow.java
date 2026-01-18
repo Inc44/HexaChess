73d0b12d19de7bd3707dc3b1dd4ef9fa39d95e83
Inc44 <incognita44@outlook.com>
2026-01-18 21:02:57 +0100

    bugfix, naming fix

--- src/main/java/im/bpu/hexachess/LeaderboardWindow.java
+++ src/main/java/im/bpu/hexachess/LeaderboardWindow.java
@@ -3,43 +3,94 @@ package im.bpu.hexachess;
 import im.bpu.hexachess.entity.Player;
 import im.bpu.hexachess.network.API;
 
+import java.io.File;
 import java.util.List;
+import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 
 import static im.bpu.hexachess.Main.loadWindow;
 
 public class LeaderboardWindow {
+	private static final String AVATAR_URL =
+		"https://www.chess.com/bundles/web/images/noavatar_l.gif";
+	private static final String FLAGS_URL =
+		"https://www.chess.com/bundles/web/images/sprites/flags-128.png";
 	@FXML private VBox leaderboardContainer;
 	@FXML
 	public void initialize() {
+		loadLeaderboard();
+	}
+	private void loadLeaderboard() {
 		Thread.ofVirtual().start(() -> {
+			final ResourceBundle bundle = Main.getBundle();
 			final List<Player> players = API.leaderboard();
 			Platform.runLater(() -> {
 				int rank = 1;
 				for (final Player player : players) {
-					final HBox row = new HBox(20);
-					row.setAlignment(Pos.CENTER_LEFT);
-					row.getStyleClass().add("player-item");
-					final Label rankLabel = new Label("#" + rank);
-					rankLabel.getStyleClass().add("leaderboard-rank");
-					final Label nameLabel = new Label(player.getHandle());
-					nameLabel.getStyleClass().add("leaderboard-name");
-					final Label eloLabel = new Label(player.getRating() + " pts");
-					eloLabel.getStyleClass().add("leaderboard-elo");
-					row.getChildren().addAll(rankLabel, nameLabel, eloLabel);
-					leaderboardContainer.getChildren().add(row);
-					rank++;
+					try {
+						final FXMLLoader playerItemLoader =
+							new FXMLLoader(getClass().getResource("ui/playerItem.fxml"), bundle);
+						final HBox playerItem = playerItemLoader.load();
+						final String handle = player.getHandle();
+						final int rating = player.getRating();
+						final String location = player.getLocation();
+						final String avatarUrl =
+							(player.getAvatar() != null && !player.getAvatar().isEmpty())
+							? player.getAvatar()
+							: AVATAR_URL;
+						final ImageView avatarIcon = (ImageView) playerItem.lookup("#avatarIcon");
+						final Label handleLabel = (Label) playerItem.lookup("#handleLabel");
+						final Region countryFlagIcon =
+							(Region) playerItem.lookup("#countryFlagIcon");
+						final Label ratingLabel = (Label) playerItem.lookup("#ratingLabel");
+						final Button challengeButton =
+							(Button) playerItem.lookup("#challengeButton");
+						challengeButton.setManaged(false);
+						challengeButton.setVisible(false);
+						final File avatarFile = CacheManager.save("avatars", handle, avatarUrl);
+						final Image avatarImage = new Image(avatarFile.toURI().toString());
+						final String flagsFileName =
+							FLAGS_URL.substring(FLAGS_URL.lastIndexOf('/') + 1);
+						final File flagsFile =
+							CacheManager.save("images", flagsFileName, FLAGS_URL);
+						avatarIcon.setImage(avatarImage);
+						handleLabel.setText(handle);
+						ratingLabel.setText(bundle.getString("common.rating") + ": " + rating);
+						if (location != null && !location.isEmpty()) {
+							countryFlagIcon.setStyle("-fx-background-image: url('"
+								+ flagsFile.toURI().toString() + "');");
+							countryFlagIcon.getStyleClass().add("country-" + location);
+							countryFlagIcon.setManaged(true);
+							countryFlagIcon.setVisible(true);
+						}
+						final Label rankLabel = new Label("#" + rank);
+						rankLabel.getStyleClass().add("leaderboard-rank");
+						playerItem.getChildren().add(0, rankLabel);
+						playerItem.setOnMouseClicked(event -> {
+							ProfileWindow.targetHandle = handle;
+							loadWindow(
+								"ui/profileWindow.fxml", new ProfileWindow(), leaderboardContainer);
+						});
+						leaderboardContainer.getChildren().add(playerItem);
+						rank++;
+					} catch (final Exception exception) {
+						exception.printStackTrace();
+					}
 				}
 			});
 		});
 	}
 	@FXML
-	private void goBack() {
+	private void handleBack() {
 		loadWindow("ui/mainWindow.fxml", new MainWindow(), leaderboardContainer);
 	}
 }
--- src/main/java/im/bpu/hexachess/ParticipantsWindow.java
+++ src/main/java/im/bpu/hexachess/ParticipantsWindow.java
@@ -4,21 +4,29 @@ import im.bpu.hexachess.entity.Player;
 import im.bpu.hexachess.entity.Tournament;
 import im.bpu.hexachess.network.API;
 
+import java.io.File;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 
 import static im.bpu.hexachess.Main.loadWindow;
 
 public class ParticipantsWindow {
+	private static final String AVATAR_URL =
+		"https://www.chess.com/bundles/web/images/noavatar_l.gif";
+	private static final String FLAGS_URL =
+		"https://www.chess.com/bundles/web/images/sprites/flags-128.png";
 	public static Tournament targetTournament;
-	@FXML private Label titleLabel;
+	@FXML private Label tournamentLabel;
 	@FXML private VBox participantsContainer;
 	@FXML private Button backButton;
 	@FXML
@@ -28,25 +36,68 @@ public class ParticipantsWindow {
 			return;
 		}
 		final ResourceBundle bundle = Main.getBundle();
-		titleLabel.setText(
-			bundle.getString("tournament.participants.title") + ": " + targetTournament.getName());
+		final String name = targetTournament.getName();
+		tournamentLabel.setText(bundle.getString("tournament.participants.title") + ": " + name);
+		loadParticipants();
+	}
+	private void loadParticipants() {
 		Thread.ofVirtual().start(() -> {
-			final List<Player> players = API.participants(targetTournament.getTournamentId());
+			final ResourceBundle bundle = Main.getBundle();
+			final String tournamentId = targetTournament.getTournamentId();
+			final List<Player> players = API.participants(tournamentId);
 			Platform.runLater(() -> {
-				participantsContainer.getChildren().clear();
 				if (players.isEmpty()) {
-					participantsContainer.getChildren().add(
-						new Label(bundle.getString("tournament.participants.empty")));
+					final Label emptyLabel =
+						new Label(bundle.getString("tournament.participants.empty"));
+					participantsContainer.getChildren().add(emptyLabel);
 				} else {
 					for (final Player player : players) {
-						final HBox row = new HBox(20);
-						row.getStyleClass().add("participant-row");
-						row.setAlignment(Pos.CENTER_LEFT);
-						final Label name = new Label(player.getHandle());
-						name.getStyleClass().add("participant-name");
-						final Label rating = new Label(player.getRating() + " ELO");
-						row.getChildren().addAll(name, rating);
-						participantsContainer.getChildren().add(row);
+						try {
+							final FXMLLoader playerItemLoader = new FXMLLoader(
+								getClass().getResource("ui/playerItem.fxml"), bundle);
+							final HBox playerItem = playerItemLoader.load();
+							final String handle = player.getHandle();
+							final int rating = player.getRating();
+							final String location = player.getLocation();
+							final String avatarUrl =
+								(player.getAvatar() != null && !player.getAvatar().isEmpty())
+								? player.getAvatar()
+								: AVATAR_URL;
+							final ImageView avatarIcon =
+								(ImageView) playerItem.lookup("#avatarIcon");
+							final Label handleLabel = (Label) playerItem.lookup("#handleLabel");
+							final Region countryFlagIcon =
+								(Region) playerItem.lookup("#countryFlagIcon");
+							final Label ratingLabel = (Label) playerItem.lookup("#ratingLabel");
+							final Button challengeButton =
+								(Button) playerItem.lookup("#challengeButton");
+							challengeButton.setManaged(false);
+							challengeButton.setVisible(false);
+							final File avatarFile = CacheManager.save("avatars", handle, avatarUrl);
+							final Image avatarImage = new Image(avatarFile.toURI().toString());
+							final String flagsFileName =
+								FLAGS_URL.substring(FLAGS_URL.lastIndexOf('/') + 1);
+							final File flagsFile =
+								CacheManager.save("images", flagsFileName, FLAGS_URL);
+							avatarIcon.setImage(avatarImage);
+							handleLabel.setText(handle);
+							ratingLabel.setText(bundle.getString("common.rating") + ": " + rating);
+							if (location != null && !location.isEmpty()) {
+								countryFlagIcon.setStyle("-fx-background-image: url('"
+									+ flagsFile.toURI().toString() + "');");
+								countryFlagIcon.getStyleClass().add("country-" + location);
+								countryFlagIcon.setManaged(true);
+								countryFlagIcon.setVisible(true);
+							}
+							playerItem.setOnMouseClicked(event -> {
+								ProfileWindow.targetHandle = handle;
+								loadWindow("ui/profileWindow.fxml", new ProfileWindow(),
+									participantsContainer);
+							});
+							participantsContainer.getChildren().add(playerItem);
+						} catch (final Exception exception) {
+							exception.printStackTrace();
+						}
 					}
 				}
 			});
